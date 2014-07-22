71ff3e55a038b02da682306f162db3941f3b62db
Morgan Phillips <morgan@artistgrowth.com>
2014-07-21 23:57:05 -0500

    Adds comments to scripts.

--- ansible-vagrant.sh
+++ ansible-vagrant.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+####
+## A nice launch shortcut for running ansible playbooks against Ansible hosts
+## on OSX
+####
+
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 PRIVATE_KEY_FILE=`find /Applications/Vagrant/ | grep "/keys/vagrant$"`
