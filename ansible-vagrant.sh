8d67239c0d5ac8f26fea840e0d4db8129e0cb400
Morgan Phillips <morgan@artistgrowth.com>
2014-07-24 01:47:07 -0500

    Makes ansible-vagrant usable on Linux.

--- ansible-vagrant.sh
+++ ansible-vagrant.sh
@@ -1,11 +1,18 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 ####
 ## A nice launch shortcut for running ansible playbooks against Ansible hosts
 ## on OSX
 ####
 
-export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_HOST_KEY_CHECKING=False;
 
-PRIVATE_KEY_FILE=`find /Applications/Vagrant/ | grep "/keys/vagrant$"`
+if [ `uname` = "Darwin" ]; then
+    PRIVATE_KEY_FILE=`find /Applications/Vagrant/ | grep "/keys/vagrant$"`;
+else
+    # Oy vey, pretty ugly atm but not sure where this will end up on Linux distros
+    pushd /;
+    PRIVATE_KEY_FILE=`find -name vagrant 2> /dev/null| grep "/keys/vagrant$"`;
+    popd;
+fi
 /usr/bin/env ansible-playbook --private-key=$PRIVATE_KEY_FILE "$@"
