42211e9094f95f59fd64af6e5a6e878051a17a43
Morgan Phillips <morgan@artistgrowth.com>
2014-07-24 19:32:59 -0500

    Makes vagrant ansible-playbook launcher host agnostic.

--- ansible-vagrant.sh
+++ ansible-vagrant.sh
@@ -6,13 +6,5 @@
 ####
 
 export ANSIBLE_HOST_KEY_CHECKING=False;
-
-if [ `uname` = "Darwin" ]; then
-    PRIVATE_KEY_FILE=`find /Applications/Vagrant/ | grep "/keys/vagrant$"`;
-else
-    # Oy vey, pretty ugly atm but not sure where this will end up on Linux distros
-    pushd /;
-    PRIVATE_KEY_FILE=`find -name vagrant 2> /dev/null| grep "/keys/vagrant$"`;
-    popd;
-fi
+PRIVATE_KEY_FILE=~/.vagrant.d/insecure_private_key
 /usr/bin/env ansible-playbook --private-key=$PRIVATE_KEY_FILE "$@"
